ebb3a707f546e239764dd495c7ef3b1a571dc8a3
Kleyver <kleyvercell2@gmail.com>
2026-04-05 10:48:07 -0400

    feat: finalización de i18n, resaltado de IDEA y ajustes finales en Hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
   vite: {
     plugins: [tailwindcss()]
   }
